646d742cf2da7c9b747a7d86d7c217ef242f3348
André Brito Vita <andrebritovita.dev@hotmail.com>
2024-09-11 15:21:37 -0300

    Valor da variável soma.

---
+++ src/main/java/ValorVariavelSoma.java
@@ -0,0 +1,15 @@
+package src.main.java;
+
+public class ValorVariavelSoma {
+    public static void main(String[] args) {
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        while (k < indice) {
+            k = k + 1;
+            soma = soma + k;
+        }
+        System.out.println("O valor da variável SOMA é " + soma + ".");
+    }
+}
